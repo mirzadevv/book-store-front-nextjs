e740ccfb75a015ba05fc7694a8084207c57a46e9
mirzaei.dev <mirzaei.devv@gmail.com>
2022-02-09 23:27:29 +0330

    styles: use mainHeader styles into the component

--- components/navigation/mainHeader.jsx
+++ components/navigation/mainHeader.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "../../styles/components/navigation/mainHeader.module.css";
 const MainHeader = (props) => {
   return (
     <>
-      <header>{props.children}</header>
+      <header className={styles.mainHeader}>{props.children}</header>
     </>
   );
 };
