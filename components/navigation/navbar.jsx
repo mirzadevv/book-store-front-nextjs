9b233c2299171cfc241e661539d52a366f26e548
mirzaei.dev <mirzaei.devv@gmail.com>
2022-02-09 23:17:41 +0330

    create navbar component
    in this component, it's been used Link and "a" tag to have a connection to other pages

---
+++ components/navigation/navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/books">
+            <a> Books </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/authors">
+            <a> Authors </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/publishers">
+            <a> Publishers </a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
