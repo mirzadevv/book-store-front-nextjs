5aca14ad7e3043809767fad4813c6ec86cc4a0a6
mirzaei.dev <mirzaei.devv@gmail.com>
2022-02-09 23:42:28 +0330

    feat: use sideDrawer component into the mainNavigation component
    for having a responsive design we have to use sideDrawer when the screen is in mobile mode, it's been shown or hidden by controlling the drawerMode state

--- components/navigation/mainNavigation.jsx
+++ components/navigation/mainNavigation.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../../styles/components/navigation/mainNavigation.module.css";
 import MainHeader from "./mainHeader";
 import Navbar from "./navbar";
+import SideDrawer from "./sideDrawer";
 import Link from "next/link";
-import { FaShoppingBasket } from "react-icons/fa";
+import { FaShoppingBasket, FaWindowClose } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 
 const MainNavigation = () => {
+  const [drawerMode, setDrawerMode] = useState(false);
+
   return (
     <>
+      <SideDrawer drawerMode={drawerMode}>
+        <div className={styles.closeBtn}>
+          <FaWindowClose
+            size={30}
+            style={{ cursor: "pointer", marginTop: "-1rem" }}
+            onClick={() => setDrawerMode(false)}
+          />
+        </div>
+        <div className={styles.navbarDrawer}>
+          <Navbar />
+        </div>
+      </SideDrawer>
       <MainHeader>
         <div className={styles.mainNavigation}>
           <div className={styles.leftSide}>
@@ -21,6 +38,11 @@ const MainNavigation = () => {
           </div>
           <div className={styles.rightSide}>
             <FaShoppingBasket size={25} className="cursorPointer" />
+            <FaBars
+              size={25}
+              className={styles.showMode}
+              onClick={() => setDrawerMode(true)}
+            />
           </div>
         </div>
       </MainHeader>
