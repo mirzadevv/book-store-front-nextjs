82cdb6be8aa898978e381c67be6142ce579cb4b5
mirzaei.dev <mirzaei.devv@gmail.com>
2022-03-03 17:05:00 +0330

    feat: use swiper package as a slider to show the bunch of books
    Swiper component gets booksData prop as an array of objects and pasEss every object to into the SwiperSlide to every bookCard component.

---
+++ components/uiElements/swiper.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination, Autoplay } from "swiper";
+import BookCard from "../uiElements/bookCard";
+
+SwiperCore.use([Pagination, Autoplay]);
+const MainSwiper = ({ booksData }) => {
+  return (
+    <Swiper
+      autoplay={{
+        delay: 2500,
+      }}
+      pagination={{
+        clickable: true,
+        type: "progressbar",
+      }}
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        600: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        900: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1000: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      }}
+    >
+      {booksData?.map((item) => (
+        <SwiperSlide key={item.id}>
+          <BookCard bookData={item} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default MainSwiper;
