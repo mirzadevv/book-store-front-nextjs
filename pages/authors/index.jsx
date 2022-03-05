ae0fea250c3f544b935e83a001bbcf3764c09856
mirzaei.dev <mirzaei.devv@gmail.com>
2022-03-05 23:35:02 +0330

    create primary author page with its styles

---
+++ pages/authors/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../styles/pages/authors/index.module.css";
+
+const Authors = () => {
+  return (
+    <div className={styles.author}>
+      <h4>AUTHORS PAGE</h4>
+    </div>
+  );
+};
+
+export default Authors;
