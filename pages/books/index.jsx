d4aff489015ad2ab9dee34bdcb13bb13bdefe6af
mirzaei.dev <mirzaei.devv@gmail.com>
2022-03-05 23:32:38 +0330

    create the primary page of books with its styles

---
+++ pages/books/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../../styles/pages/books/index.module.css";
+const Books = () => {
+  return (
+    <div className={styles.books}>
+      <h3>BOOKS PAGE</h3>
+    </div>
+  );
+};
+
+export default Books;
