68fe8f910acad0131bc6d30abc42f134b32baa0c
mirzaei.dev <mirzaei.devv@gmail.com>
2022-03-05 23:28:28 +0330

    feat: add Link to every bookCard for going to their page

--- components/uiElements/bookCard.jsx
+++ components/uiElements/bookCard.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../../styles/components/uiElements/bookCard.module.css";
 import { API_URL } from "../../config/index";
 
 const BookCard = ({ bookData }) => {
   const srcImage = `${API_URL}${bookData?.attributes?.image?.data[0]?.attributes?.url}`;
-  const category = bookData?.attributes?.category?.data?.attributes?.title;
+  const categoryTilte = bookData?.attributes?.category?.data?.attributes?.title;
 
   return (
-    <div className={styles.bookCard}>
-      <div className={styles.image}>
-        <Image
-          loader={() => {
-            return srcImage;
-          }}
-          src={srcImage}
-          width={300}
-          height={400}
-        />
-        <div className={styles.category}>
-          <h3>{!category ? "undefined" : category}</h3>
-        </div>
-      </div>
-    </div>
+    <>
+      <Link href={`/books/${bookData?.id}`}>
+        <a>
+          <div className={styles.bookCard}>
+            <div className={styles.image}>
+              <Image
+                loader={() => {
+                  return srcImage;
+                }}
+                src={srcImage}
+                width={300}
+                height={400}
+              />
+              <div className={styles.category}>
+                <h3>{!categoryTilte ? "undefined" : categoryTilte}</h3>
+              </div>
+            </div>
+          </div>
+        </a>
+      </Link>
+    </>
   );
 };
 
