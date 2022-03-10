0d14672e0bf188aa61c3f85074d28f918ec185f2
mirzaei.dev <mirzaei.devv@gmail.com>
2022-03-10 21:21:24 +0330

    feature: add type property to the component with its styles
    Based on this property, we determine whether the component is small or large, which in the large case, in addition to the large image of the component, also shows the title of the category, and in the small case, in addition to the small image of the component, shows the title of the book.

--- components/uiElements/bookCard.jsx
+++ components/uiElements/bookCard.jsx
@@ -4,9 +4,57 @@ import Link from "next/link";
 import styles from "../../styles/components/uiElements/bookCard.module.css";
 import { API_URL } from "../../config/index";
 
-const BookCard = ({ bookData }) => {
+const BookCard = ({ bookData, type }) => {
   const srcImage = `${API_URL}${bookData?.attributes?.image?.data[0]?.attributes?.url}`;
   const categoryTilte = bookData?.attributes?.category?.data?.attributes?.title;
+  const bookTitle = bookData?.attributes?.title;
+
+  function getImage() {
+    if (type === "large") {
+      return (
+        <Image
+          loader={() => {
+            return srcImage;
+          }}
+          src={srcImage}
+          width={300}
+          height={400}
+        />
+      );
+    } else if (type === "small") {
+      return (
+        <Image
+          loader={() => {
+            return srcImage;
+          }}
+          src={srcImage}
+          width={180}
+          height={220}
+        />
+      );
+    }
+  }
+
+  function getCategoryOrTitle() {
+    const headingStyles = {
+      marginTop: "6px",
+      color: "white",
+      textTransform: "uppercase",
+    };
+    return (
+      <div className={styles.imageTitle}>
+        {type === "large" ? (
+          <h3 style={headingStyles}>
+            {!categoryTilte ? "undefined" : categoryTilte}
+          </h3>
+        ) : (
+          <h3 style={{ ...headingStyles, fontSize: "12px" }}>
+            {!categoryTilte ? "undefined" : bookTitle}
+          </h3>
+        )}
+      </div>
+    );
+  }
 
   return (
     <>
@@ -14,17 +62,8 @@ const BookCard = ({ bookData }) => {
         <a>
           <div className={styles.bookCard}>
             <div className={styles.image}>
-              <Image
-                loader={() => {
-                  return srcImage;
-                }}
-                src={srcImage}
-                width={300}
-                height={400}
-              />
-              <div className={styles.category}>
-                <h3>{!categoryTilte ? "undefined" : categoryTilte}</h3>
-              </div>
+              {getImage()}
+              {getCategoryOrTitle()}
             </div>
           </div>
         </a>
