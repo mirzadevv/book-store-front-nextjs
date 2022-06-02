9bb4ae5975153959185118174f48afe522d56655
mirzaei.dev <mirzaei.devv@gmail.com>
2022-06-03 00:39:05 +0430

    feat: add onClick prop to button component

--- components/uiElements/button.jsx
+++ components/uiElements/button.jsx
@@ -1,6 +1,11 @@
 import styles from "../../styles/components/uiElements/button.module.css";
-export default function Button({ title, type }) {
+export default function Button({ title, type, onClick }) {
   return (
-    <div className={`${styles.button} ${styles[`${type}Button`]}`}>{title}</div>
+    <div
+      className={`${styles.button} ${styles[`${type}Button`]}`}
+      onClick={() => onClick({ title })}
+    >
+      {title}
+    </div>
   );
 }
