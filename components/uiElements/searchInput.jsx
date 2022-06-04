486c23a16ec9839ca9815911981c1df5f544bc66
mirzaei.dev <mirzaei.devv@gmail.com>
2022-06-04 16:56:25 +0430

    styles: change the plcaceholder of the searchInput component

--- components/uiElements/searchInput.jsx
+++ components/uiElements/searchInput.jsx
@@ -18,7 +18,7 @@ export default function SearchInput() {
         <input
           onChange={handleChange}
           className={styles.input}
-          placeholder="Search books and so on"
+          placeholder="Search for books, categories, authors, and so on"
         />
       </form>
     </>
