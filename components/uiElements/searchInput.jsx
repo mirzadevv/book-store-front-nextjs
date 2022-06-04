a2fb5983fd55526d0534c3e2c1510f382d3c57af
mirzaei.dev <mirzaei.devv@gmail.com>
2022-06-04 16:21:13 +0430

    feat: create searchInput component

---
+++ components/uiElements/searchInput.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import styles from "../../styles/components/uiElements/searchInput.module.css";
+export default function SearchInput() {
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    router.push(`/books/search?value=${value}`);
+  }
+
+  function handleChange(e) {
+    setValue(e.currentTarget.value);
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          className={styles.input}
+          placeholder="Search books and so on"
+        />
+      </form>
+    </>
+  );
+}
