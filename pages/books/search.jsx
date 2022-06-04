5aa4c8bb13ae270fb22bfd5dadadd2b469c1ddc8
mirzaei.dev <mirzaei.devv@gmail.com>
2022-06-04 16:26:35 +0430

    feat: create a Search page
    by searching a word in the searchInput component, something like this: "search?value=Hodder" will be appeared in the route, Hodder is the value of the search input component, and an overall Search page will be shown and according the value, fetch will be done.

---
+++ pages/books/search.jsx
@@ -0,0 +1,98 @@
+import styles from "../../styles/pages/books/search.module.css";
+import { API_URL } from "../../config";
+
+export default function Search({ result }) {
+  return (
+    <div className={styles.bookSearch}>
+      {result?.data?.map((item) => (
+        <div className={styles.searchItem} key={item.id}>
+          <div
+            style={{
+              backgroundImage: `url(
+                ${API_URL}${item?.attributes?.image?.data[0]?.attributes?.url}
+              )`,
+            }}
+            className={styles.imgSide}
+          ></div>
+          <div className={styles.descriptionSide}>
+            <h4>
+              <span style={{ color: "blue" }}>Title: </span>
+              {item.attributes.title}
+            </h4>
+            <h4>
+              <span style={{ color: "blue" }}>Description: </span>
+              {item.attributes.description}
+            </h4>
+            <h4>
+              <span style={{ color: "blue" }}>Price: </span>
+              {item.attributes.price} $
+            </h4>
+            <h4>
+              <span style={{ color: "blue" }}>Author: </span>
+              {item.attributes.author.data.attributes.full_name}
+            </h4>
+            <h4>
+              <span style={{ color: "blue" }}>Category: </span>
+              <span>{item.attributes.category.data.attributes.title}</span>
+            </h4>
+            <h4>
+              <span style={{ color: "blue" }}>Publisher: </span>
+              <span>{item.attributes.publisher.data.attributes.title}</span>
+            </h4>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export async function getServerSideProps({ query: { value } }) {
+  const qs = require("qs");
+  const query = qs.stringify(
+    {
+      populate: "*",
+      filters: {
+        $or: [
+          {
+            title: {
+              $contains: value,
+            },
+          },
+          {
+            author: {
+              full_name: {
+                $contains: value,
+              },
+            },
+          },
+          {
+            category: {
+              title: {
+                $contains: value,
+              },
+            },
+          },
+          {
+            publisher: {
+              title: {
+                $contains: value,
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+
+  const response = await fetch(`${API_URL}/api/books?${query}`);
+  const result = await response.json();
+
+  return {
+    props: {
+      result,
+    },
+  };
+}
