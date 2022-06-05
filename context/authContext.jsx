e0d71d787317e293811e035b08c53cd40d49e432
mirzaei.dev <mirzaei.devv@gmail.com>
2022-06-05 16:48:42 +0430

    feat: create authContext for doing user authentication
    This context includes user and error states, it consists register, login, logout and checkUserLogin functions

---
+++ context/authContext.jsx
@@ -0,0 +1,86 @@
+import React, { createContext, useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { NEXT_URL } from "../config/index";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const router = useRouter();
+
+  // the first thing will be executed, because every context has a global provider
+  useEffect(() => {
+    checkUserLoggedIn();
+  }, []);
+
+  const register = async (user) => {
+    const res = await fetch(`${NEXT_URL}/api/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    });
+    const data = await res.json();
+    if (res.ok) {
+      // cookie will be filled
+      // data consists user and jwt(token)
+      setUser(data.user);
+      router.push("/account/dashboard");
+    } else {
+      setError(data.error.message);
+      setError(null);
+    }
+  };
+
+  const login = async ({ email: identifier, password }) => {
+    const res = await fetch(`${NEXT_URL}/api/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        identifier,
+        password,
+      }),
+    });
+    const data = await res.json();
+    if (res.ok) {
+      // cookie will be filled
+      // data consists user and jwt(token)
+      setUser(data.user);
+      router.push("/account/dashboard");
+    } else {
+      setError(data.error.message);
+      setError(null);
+    }
+  };
+
+  const logout = async () => {
+    const response = await fetch(`${NEXT_URL}/api/logout`, {
+      method: "POST",
+    });
+
+    if (response.ok) {
+      setUser(null);
+      router.push("/");
+    }
+  };
+
+  const checkUserLoggedIn = async () => {
+    //check if cookie is fill or not, in every refresh or something like that, it will be checked beacuse it's been writen in the useEffect
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setUser(null);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, error, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
